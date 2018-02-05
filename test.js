33efb73e9dee958a40706524bc4613fd37f91cf3
JoseYepez <yepez.jose719@gmail.com>
2018-02-05 13:52:25 -0500

    You can come dude js file added

--- test.js
+++ test.js
@@ -1,25 +1,12 @@
-var myAge = 49;
-if (myAge > 30){
+var myAge = 26;
+if (myAge < 18  || myAge > 30 || myAge === 25){
 
-        document.write("You are over 30!");
-
-
-}   else if (myAge > 20) {
-
-        document.write("You are over 20!");
+      document.write("You aint coming!");
 
 
+} else {
 
-}   else if (myAge > 10) {
-
-        document.write("You are over 10!");
-
-
-
-}     else{
-
-
-      document.write("You are not over 10!");
+      document.write("You can come, you cool dude");
 
 
 }
