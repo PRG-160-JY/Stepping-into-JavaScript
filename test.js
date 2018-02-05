efc9dc110b8ddb9b793c916c9aadf56196a04bad
JoseYepez <yepez.jose719@gmail.com>
2018-02-05 13:22:00 -0500

    Added you like meat js file

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-alert ("Hello, World!");
+var youLikeMeat = true;
+var myNum = 10;
+if (youLikeMeat) {
+
+  document.write("You like meat");
+
+
+}   else{
+
+
+  document.write("You hate meat");
+
+
+
+
+}
