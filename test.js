ccb38a0e4bc9f21f28e17bf5e4d707c821aa0796
JoseYepez <yepez.jose719@gmail.com>
2018-03-01 19:06:09 -0500

    Code challenge #1
    function that will write to console the average of 5 numbers

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-function getAverage (a,b,c,d,e,f) {
+function getAverage (a,b,c,d,e,) {
 	
-	var average = (a + b + c + d + e + f) / 6;
+	var average = (a + b + c + d + e) / 5;
 	console.log(average);
 	return average;
 	
 	
 }
 
-var myResult = getAverage(7,8,9,10,11,12);
+var myResult = getAverage(1,2,3,4,5);
 console.log("the average is " + myResult);
